e7fa5412c121625d5d010a2f4356cff288e8f594
isabey <isaarinc@gmail.com>
2021-10-23 15:52:58 +0300

    Added lwc Compositions (parent-child)

---
+++ force-app/main/default/lwc/lwcCompositionChild/lwcCompositionChild.js
@@ -0,0 +1,8 @@
+import { api, LightningElement } from 'lwc';
+
+export default class LwcCompositionChild extends LightningElement {
+
+    @api
+    information="This comes from information."
+    
+}
