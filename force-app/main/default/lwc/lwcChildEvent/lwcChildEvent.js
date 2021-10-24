de70a07da70b30dcb49cc632ad499d0eda4e3c6a
isabey <isaarinc@gmail.com>
2021-10-24 18:25:53 +0300

    lwcParentEvent-lwcChildEvent v2

--- force-app/main/default/lwc/lwcChildEvent/lwcChildEvent.js
+++ force-app/main/default/lwc/lwcChildEvent/lwcChildEvent.js
@@ -1,7 +1,24 @@
 import { LightningElement } from 'lwc';
 export default class LwcChildEvent extends LightningElement {
+    firstName = '';
+    lastName = '';
+    email = '';
     handleButton(event) {
-        let newCustomEvent = new CustomEvent('buttonclick');
+        let info = {
+            firstName: this.firstName,
+            lastName: this.lastName,
+            email: this.email
+        };
+        let newCustomEvent = new CustomEvent('fetchcontact', { detail: info});
         this.dispatchEvent(newCustomEvent);
+    }
+    handleFirstName(event) {
+        this.firstName = event.detail.value;
+    }
+    handleLastName(event) {
+        this.lastName = event.detail.value;
+    }
+    handleEmail(event) {
+        this.email = event.detail.value;
     }
 }
--- force-app/main/default/lwc/lwcParentEvent/lwcParentEvent.js
+++ force-app/main/default/lwc/lwcParentEvent/lwcParentEvent.js
@@ -1,7 +1,12 @@
 import { LightningElement } from 'lwc';
 export default class LwcParentEvent extends LightningElement {
+    firstName = '';
+    lastName = '';
+    email = '';
     handleChildButtonClick(event) {
         console.log('Child event was caught in parent component');
-        console.log(JSON.stringify(event));
+        this.firstName = event.detail.firstName;
+        this.lastName = event.detail.lastName;
+        this.email = event.detail.email;
     }
 }
