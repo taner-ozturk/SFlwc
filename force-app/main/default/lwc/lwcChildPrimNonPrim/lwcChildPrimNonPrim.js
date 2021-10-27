369afd799923981e30a31fec0ee0fe54731f7133
isabey <isaarinc@gmail.com>
2021-10-27 06:11:23 +0300

    added lwc parent child prim non prim v.last and bubbles and composed note.

--- force-app/main/default/lwc/lwcChildPrimNonPrim/lwcChildPrimNonPrim.js
+++ force-app/main/default/lwc/lwcChildPrimNonPrim/lwcChildPrimNonPrim.js
@@ -1,8 +1,29 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
 export default class LwcChildPrimNonPrim extends LightningElement {
+    /*
+        Keep the usage of @api and @track different
+        Use @api only to catch values from parent components and dont use it if you are expecting 
+           the property to change like @track
+        Use @track only to make dynamic changes in html
+        DO NOT create properties which you would like to have as reactive(@track) 
+            and public(@api) at the same time
+    */
     @api
     publicProperty; //This doesnt have a default
+    
+    @api
+    publicPropertyObject; //This doesnt have a default
+    showPublicProperty = false;
+    showPublicPropertyObject = false;
+    @track listAccount = [];
     connectedCallback() {
-        console.log('15 public property: ', JSON.parse(JSON.stringify(this.publicProperty)));
+        if (this.publicPropertyObject) {
+            this.showPublicPropertyObject = true;
+            console.log('36 public property Object: ', JSON.parse(JSON.stringify(this.publicPropertyObject)));
+        }
+        if (this.publicProperty) {
+            this.showPublicProperty = true;
+            console.log('40 public property: ', this.publicProperty);
+        }
     }
 }
