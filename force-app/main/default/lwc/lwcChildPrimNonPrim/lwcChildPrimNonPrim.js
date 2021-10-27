492af3ab0dcc54328d94ab417d4d72cf216bb6ea
isabey <isaarinc@gmail.com>
2021-10-27 05:09:14 +0300

    primnonprim v2

--- force-app/main/default/lwc/lwcChildPrimNonPrim/lwcChildPrimNonPrim.js
+++ force-app/main/default/lwc/lwcChildPrimNonPrim/lwcChildPrimNonPrim.js
@@ -1,5 +1,8 @@
 import { api, LightningElement } from 'lwc';
 export default class LwcChildPrimNonPrim extends LightningElement {
     @api
-    publicProperty;
+    publicProperty; //This doesnt have a default
+    connectedCallback() {
+        console.log('15 public property: ', this.publicProperty);
+    }
 }
