a00ed36fd23db0430218c016ebe78e9842d22dca
isabey <isaarinc@gmail.com>
2021-10-28 04:27:46 +0300

    Added prim-non-prim vl-last+1

--- force-app/main/default/lwc/lwcParentPrimNonPrim/lwcParentPrimNonPrim.js
+++ force-app/main/default/lwc/lwcParentPrimNonPrim/lwcParentPrimNonPrim.js
@@ -1,8 +1,12 @@
 import { LightningElement, track } from 'lwc';
 export default class LwcParentPrimNonPrim extends LightningElement {
+    primitiveString = 'This is Wednesday night';
     @track
     contactRec = {
         firstName : 'Selcuk',
         lastName : 'A'
+    }
+    handleTextChange(event) {
+        this.primitiveString = event.detail.value;
     }
 }
