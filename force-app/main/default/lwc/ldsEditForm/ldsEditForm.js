53976d57e8d3a6cdaecf060bd5cf0d5291186a35
isabey <isaarinc@gmail.com>
2021-11-04 05:19:54 +0300

    ldsEditForm v2

--- force-app/main/default/lwc/ldsEditForm/ldsEditForm.js
+++ force-app/main/default/lwc/ldsEditForm/ldsEditForm.js
@@ -1,5 +1,14 @@
-import { LightningElement , api } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Lead.Name';
+import EMAIL_FIELD from '@salesforce/schema/Lead.Email';
+import SOURCE_FIELD from '@salesforce/schema/Lead.LeadSource';
+import COMPANY_FIELD from '@salesforce/schema/Lead.Company';
 export default class LdsEditForm extends LightningElement {
     @api recordId;
     @api objectApiName = "Lead";
+    
+    nameField = NAME_FIELD;
+    emailField = EMAIL_FIELD;
+    sourceField = SOURCE_FIELD;
+    companyField = COMPANY_FIELD;
 }
