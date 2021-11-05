843f318954cf88d7bf8ebc49748e6d42ab32986f
isabey <isaarinc@gmail.com>
2021-11-05 04:59:15 +0300

    ldsForm for homepage

--- force-app/main/default/lwc/ldsForm/ldsForm.js
+++ force-app/main/default/lwc/ldsForm/ldsForm.js
@@ -1,7 +1,8 @@
 import { LightningElement , api } from 'lwc';
 export default class LdsForm extends LightningElement {
     @api recordId;
-    @api objectApiName;
+    //@api objectApiName;
+    @api objectApiName="Lead";
     
     layoutType = 'Full'; //Compact
     fields = ['Name', 'Email', 'Company', 'LeadSource'];
