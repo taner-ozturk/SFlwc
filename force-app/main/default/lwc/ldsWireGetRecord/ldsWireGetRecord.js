ae0106a952e48593cca86f1fff40a7d0f437b698
isabey <isaarinc@gmail.com>
2021-11-06 21:39:45 +0300

    ldsWireGetRecord v2

--- force-app/main/default/lwc/ldsWireGetRecord/ldsWireGetRecord.js
+++ force-app/main/default/lwc/ldsWireGetRecord/ldsWireGetRecord.js
@@ -1,19 +1,38 @@
-import { LightningElement, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { LightningElement, api, wire, track } from 'lwc';
+import { getFieldValue, getRecord } from 'lightning/uiRecordApi';
+import MOBILE_FIELD from '@salesforce/schema/Contact.MobilePhone';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import DEP_FIELD from '@salesforce/schema/Contact.Department';
 export default class LdsWireGetRecord extends LightningElement {
     @api recordId;
     name;
-    email;
-    mobile;
-    @wire(getRecord, { recordId: '$recordId', fields: ['Contact.Name', 'Contact.Email','Contact.MobilePhone'] })
+    // email;
+    // mobile;
+    @track contact;
+    FIELDS = [NAME_FIELD, MOBILE_FIELD, EMAIL_FIELD, DEP_FIELD];
+    @wire(getRecord, { recordId: '$recordId', fields: '$FIELDS'})
     getRecordInfo({ error, data }) {
         if (data) {
             console.log('Data from get record: ' + JSON.stringify(data));
-            this.name = data.fields.Name.value;
-            this.email = data.fields.Email.value;
-            this.mobile = data.fields.MobilePhone.value;
+            this.contact = data;
+            this.name = getFieldValue(this.contact, NAME_FIELD);
+            // this.email = data.fields.Email.value;
+            // this.mobile = data.fields.MobilePhone.value;
         } else if (error) {
             console.log('Error from get record: ' + JSON.stringify(error));
         }
+    }
+    // get name() {
+    //     return getFieldValue(this.contact, NAME_FIELD);
+    // }
+    get email() {
+        return getFieldValue(this.contact, EMAIL_FIELD);
+    }
+    get mobile() {
+        return getFieldValue(this.contact, MOBILE_FIELD);
+    }
+    get dept() {
+        return getFieldValue(this.contact, DEP_FIELD);
     }
 }
