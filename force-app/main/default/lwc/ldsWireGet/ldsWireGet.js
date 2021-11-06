d0655664c3e15b3b2555f3ca0eed4285934c1237
isabey <isaarinc@gmail.com>
2021-11-06 20:40:14 +0300

    ldsWireGet new version

--- force-app/main/default/lwc/ldsWireGet/ldsWireGet.js
+++ force-app/main/default/lwc/ldsWireGet/ldsWireGet.js
@@ -1,20 +1,20 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import { getObjectInfo, getObjectInfos, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo, getObjectInfos, getPicklistValues, getPicklistValuesByRecordType } from 'lightning/uiObjectInfoApi';
 import LEAD_OBJECT from '@salesforce/schema/Lead';
 import CONTACT_OBJECT from '@salesforce/schema/Contact';
 import LEAD_SOURCE_FIELD from '@salesforce/schema/Contact.LeadSource';
-import City_FIELD from '@salesforce/schema/Contact.City__c';
+import CITY_FIELD from '@salesforce/schema/Contact.City__c';
 export default class LdsWireGet extends LightningElement {
     @track objectInfoProperty;
     @wire(getObjectInfo, { objectApiName: LEAD_OBJECT })
     objectInfoFunction({ error, data }) {
         if (data) {
             this.objectInfoProperty = data;
-            console.log('This is the object Info: ', JSON.stringify(data));
-            console.log('This is the object Prefix: ', data.keyPrefix);
-            console.log('This is the object Label: ', data.label);
+            // console.log('This is the object Info: ', JSON.stringify(data));
+            // console.log('This is the object Prefix: ', data.keyPrefix);
+            // console.log('This is the object Label: ', data.label);
             
-            console.log('This is the object\'s field: ', data.fields.AnnualRevenue);
+            // console.log('This is the object\'s field: ', data.fields.AnnualRevenue);
         } else if (error) {
             
         }
@@ -22,25 +22,33 @@ export default class LdsWireGet extends LightningElement {
     @wire(getObjectInfos, { objectApiNames: [LEAD_OBJECT, CONTACT_OBJECT] })
     objectInfosFunction({ error, data }) {
         if (data) {
-            console.log('This is the objects Info: ', JSON.stringify(data));
+            // console.log('This is the objects Info: ', JSON.stringify(data));
             for (let i = 0; i < data.results.length; i++){
                 let objectInfo = data.results[i];
                 if (objectInfo.statusCode === 200) {
                     let objectResult = objectInfo.result;
-                    console.log('This is the object Prefix: ', objectResult.keyPrefix);
-                    console.log('This is the object Label: ', objectResult.label);
+                    // console.log('This is the object Prefix: ', objectResult.keyPrefix);
+                    // console.log('This is the object Label: ', objectResult.label);
                 }
             }
         } else if (error) {
             
         }
     }
-    @wire(getPicklistValues, { recordTypeId: '0124L000000ph4yQAA', fieldApiName: City_FIELD })
+    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: CITY_FIELD })
     picklistValueInfo({ error, data }) {
         if (data) {
-            console.log('This is the picklist Info: ', JSON.stringify(data));
+            // console.log('This is the picklist Info: ', JSON.stringify(data));
         } else if (error) {
-            console.log('Error in picklist fetch: ', JSON.stringify(error));
+            // console.log('Error in picklist fetch: ', JSON.stringify(error));
+        }
+    }
+    @wire(getPicklistValuesByRecordType, { objectApiName: CONTACT_OBJECT, recordTypeId: '0124L000000ph4yQAA' })
+    picklistValueInfoByRecordType({ error, data }) {
+        if (data) {
+            console.log('This is the picklist by RT Info: ', JSON.stringify(data));
+        } else if (error) {
+            console.log('Error in picklist by RT fetch: ', JSON.stringify(error));
         }
     }
 }
