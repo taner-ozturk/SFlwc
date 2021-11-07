021fa024cf02249d8ab02a281aecad74f2635af3
isabey <isaarinc@gmail.com>
2021-11-07 18:40:46 +0300

    ldsCreateRecord with validations

--- force-app/main/default/lwc/ldsCreateRecord/ldsCreateRecord.js
+++ force-app/main/default/lwc/ldsCreateRecord/ldsCreateRecord.js
@@ -9,7 +9,15 @@ export default class LdsCreateRecord extends LightningElement {
     name;
     stageName;
     closeDate;
+    todayDate;
     @track stageOptions;
+    connectedCallback() {
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        let yyyy = today.getFullYear();
+        this.todayDate = mm + '/' + dd + '/' + yyyy;
+    }
     @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: STAGE_NAME })
     stageNamePicklistInfo({ error, data }) {
         if (data) {
@@ -33,6 +41,23 @@ export default class LdsCreateRecord extends LightningElement {
         this.closeDate = event.detail.value;
     }
     handleCreateOpp(event) {
+        const allValidInputs = [
+            ...this.template.querySelectorAll('lightning-input'),
+        ].reduce((validSoFar, inputCmp) => {
+            inputCmp.reportValidity();
+            return validSoFar && inputCmp.checkValidity();
+        }, true);
+        const allValidPicklists = [
+            ...this.template.querySelectorAll('lightning-combobox'),
+        ].reduce((validSoFar, inputCmp) => {
+            inputCmp.reportValidity();
+            return validSoFar && inputCmp.checkValidity();
+        }, true);
+        if (!allValidInputs || !allValidPicklists) {
+            alert('Please fix the issues');
+            return;
+        }
+        
         //external API hitting SF
         //it provides field values=> integrationRecord
         const fields = {};
@@ -43,7 +68,7 @@ export default class LdsCreateRecord extends LightningElement {
         createRecord(oppRecord)
             .then(opportunityRecord => {
                 console.log('opportunityRecord: ', JSON.stringify(opportunityRecord));
-                alert('Opportunity created with Id: ' + opportunityRecord.id);
+                alert('Opportunity created with Id: '+ opportunityRecord.id);
             })
             .catch(error => {
                 alert('Creation failed');
